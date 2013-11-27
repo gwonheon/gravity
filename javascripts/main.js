ffd70140215df76471513f20e1095d8200321f36
gwonheon <pafmm653@naver.com>
2013-11-27 16:23:04 +0900

    1

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
-console.log('This would be the main JS file.');
+var scene;
+
+var init = function() {
+    scene = document.querySelector('#scene');    
+};
+
+window.onload = function() {
+    init();  
+};
+
+window.onmousemove = function(e) {
+    var dx = e.pageX;
+    var dy = e.pageY;
+    var s = 'rotateY(' + dx + 'deg) rotateX(' + dy + 'deg)'; 
+    scene.style['-webkit-transform'] = s;
+};
